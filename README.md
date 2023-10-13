37d304025b9fa86ec14cabf3102551465114a95a
Abdullah Khadeli <akhadeli@ualberta.ca>
2023-10-13 12:05:28 -0600

    finished

--- README.md
+++ README.md
@@ -3,12 +3,49 @@ CMPUT404-assignment-web-client
 
 CMPUT404-assignment-web-client
 
+Abdullah Khadeli
+
+SID: 1722102
+
+CCID: akhadeli
+
 See requirements.org (plain-text) for a description of the project.
 
 Make a simple web-client like curl or wget
 
+Collaboration / Sources
+=======================
+
+Collaboration:
+    Jaskirat Singh Saggu -  I helped within the bounds of
+                            the collaboration policy
+
+Sources used:
+
+https://docs.python.org/3/library/urllib.parse.html
+https://developer.mozilla.org/en-US/
+https://datatracker.ietf.org/doc/html/rfc2616
+https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html
+https://www.w3schools.com/python/python_regex.asp
+
+Class notes/discussion
+Previous labs/assignments
+
 Contributors / Licensing
 ========================
+LICENSED under the Apache 2 license by Abdullah Khadeli
+
+httpclient.py contains contributions from:
+
+* Abdullah Khadeli
+
+httpclient.py is derived from Abram Hindle and contributors' work
+and thus some code is Copyright © 2016 2016 Abram Hindle, 
+https://github.com/tywtyw2002, and https://github.com/treedust
+
+https://github.com/uofa-cmput404/assignment-web-client
+
+Previously licensed under Abram Hindle (See below)
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle, 
 https://github.com/tywtyw2002, and https://github.com/treedust
--- httpclient.py
+++ httpclient.py
@@ -24,6 +24,7 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -41,13 +42,29 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        '''Return response status code as integer'''
+        if data:
+            return int(data.split('\r\n')[0].split()[1])
+        else:
+            return 404
 
     def get_headers(self,data):
-        return None
+        '''Return response headers in a dictionary'''
+        headers = {}
+        if data:
+            for header in data.split('\r\n\r\n')[0].split('\r\n')[1:]:
+                idx = re.search(":", header).start()
+                headers[header[0:idx]] = header[idx+2:]
+            print(headers)
+        else:
+            return ""
 
     def get_body(self, data):
-        return None
+        '''Return response body as a string'''
+        if data:
+            return data.split('\r\n\r\n')[1]
+        else:
+            return ""
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -66,15 +83,129 @@ class HTTPClient(object):
             else:
                 done = not part
         return buffer.decode('utf-8')
+    
+    def header_builder(self, method, host, path, headers_dict, args = "", query = ""):
+        '''Header builder for GET and POST'''
+
+        # setting up query if it exists
+        if query != "":
+            path += "?{}".format(query)
+        headers = "{} {} HTTP/1.1\r\n".format(method, path)
+        headers += "Host: {}\r\n".format(host)
+        
+        for header in headers_dict:
+            headers += "{}: {}\r\n".format(header, headers_dict[header])
+        
+        headers += '\r\n'
+        
+        # args provided through forms
+        if args:
+            headers += args
+        else:
+            headers += urllib.parse.urlencode("")
+
+        return headers
 
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        # parsed URL
+        parsed = urllib.parse.urlparse(url)
+
+        host = parsed.netloc.split(':')[0]
+        
+        # HTTP requirement
+        port = parsed.port
+        if port is None:
+            port = 80
+        
+        # path fixing
+        path = parsed.path
+        if path == "":
+            path = "/"
+
+        # args encoding
+        if args:
+            args = urllib.parse.urlencode(args)
+        
+        # exception handling if host is invalid or any other exceptions
+        try:
+            self.connect(host, port)
+            headers = self.header_builder("GET", host, path, {
+                'Accept': '*/*',
+                'Connection': 'close'
+            }, args, query=parsed.query)
+            self.sendall(headers)
+            res = self.recvall(self.socket)
+            
+            # As a user when I GET or POST I want the result printed to stdout
+            print(res)
+
+            # setting up for return at the end
+            code = self.get_code(res)
+            body = self.get_body(res)
+
+        except Exception as e:
+            print("ERROR:", e)
+            code = 404
+        
+        # closing socket
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        # parsed URL
+        parsed = urllib.parse.urlparse(url)
+
+        host = parsed.netloc.split(':')[0]
+
+        # HTTP requirement
+        port = parsed.port
+        if port is None:
+            port = 80
+        
+        # path fixing
+        path = parsed.path
+        if path == "":
+            path = "/"
+
+        # encoding args and getting length
+        args_length = 0
+        if args:
+            args = urllib.parse.urlencode(args)
+            args_length = len(args)
+
+        # exception handling if host is invalid or any other exceptions
+        try:
+            self.connect(host, port)
+
+            # content length included
+            headers = self.header_builder("POST", host, path, {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': '{}'.format(args_length),
+                'Connection': 'close'
+            }, args, query=parsed.query)
+            self.sendall(headers)
+            res = self.recvall(self.socket)
+
+            # As a user when I GET or POST I want the result printed to stdout
+            print(res)
+
+            code = self.get_code(res)
+            body = self.get_body(res)
+
+        except Exception as e:
+            print("ERROR:", e)
+            code = 404
+
+        # closing socket
+        self.close()
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
